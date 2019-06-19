f042495a9e63fb56c648810cbdab8d625fc40864
Hearty Oh <heartyoh@hatiolab.com>
2019-06-19 12:39:35 +0900

    v0.2.0
    ejs 적용.

--- server/index.ts
+++ server/index.ts
@@ -3,16 +3,3 @@ export * from './migrations'
 export * from './graphql'
 
 import './routes'
-
-process.on('bootstrap-module-middleware' as any, app => {
-  const render = require('koa-ejs')
-  const path = require('path')
-
-  render(app, {
-    root: path.join(__dirname, '..', 'views'),
-    layout: 'template',
-    viewExt: 'html',
-    cache: false,
-    debug: true
-  })
-})
