3e5ca2747508f443c9096ab30832559dc93e83e2
Hearty Oh <heartyoh@hatiolab.com>
2019-06-24 15:47:35 +0900

    v0.2.15

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,12 +1,9 @@
 import { store } from '@things-factory/shell'
 
-import board from './reducers/board'
 import { APPEND_PRINTER, REGISTER_PRINTER_TYPE } from '@things-factory/print-base'
 import { labelPrinterHandler } from './handlers/label-printer-handler'
 
 export default function bootstrap() {
-  store.addReducers({ board })
-
   store.dispatch({
     type: REGISTER_PRINTER_TYPE,
     printerType: 'label',
