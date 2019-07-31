aaae84990bb0362032c526df9472c971dc327f79
Hearty Oh <heartyoh@hatiolab.com>
2019-07-31 13:21:06 +0900

    v0.2.32

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,6 +1,6 @@
 import { store } from '@things-factory/shell'
 
-import { APPEND_PRINTER, REGISTER_PRINTER_TYPE } from '@things-factory/print-base'
+import { REGISTER_PRINTER_TYPE } from '@things-factory/print-base'
 import { labelPrinterHandler } from './handlers/label-printer-handler'
 
 export default function bootstrap() {
@@ -8,13 +8,5 @@ export default function bootstrap() {
     type: REGISTER_PRINTER_TYPE,
     printerType: 'label',
     handler: labelPrinterHandler
-  })
-
-  store.dispatch({
-    type: APPEND_PRINTER,
-    printer: {
-      type: 'label',
-      name: 'Zebra_Technologies_ZTC_GT800__EPL_'
-    }
   })
 }
